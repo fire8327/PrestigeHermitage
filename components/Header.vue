df446e735a9c602dd86b651b5594f5947dde9576
fire8327 <fire83274@gmail.com>
2024-05-21 15:05:16 +0300

    create index

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,25 @@
 <template>
-    <header class="w-full grid-container py-6 relative">
-        123
+    <header class="w-full grid-container py-6 relative bg-white rounded-b-3xl">
+        <div class="flex flex-col gap-4">
+            <div class="flex items-center justify-between">
+                <NuxtLink class="text-gray-500 text-base transition-all duration-500 hover:text-[#292929]">О компании</NuxtLink>
+                <NuxtLink to="tel:+7 495 126-16-38" class="text-xl">+7 495 126-16-38</NuxtLink>
+            </div>
+            <div class="w-full h-px bg-gray-400"></div>
+            <div class="flex items-center justify-between">
+                <NuxtLink to="/">
+                    <img src="/images/header/logo.png" alt="" class="w-40">
+                </NuxtLink>
+                <nav class="flex items-center gap-6 text-lg">
+                    <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Поис квартир</NuxtLink>
+                    <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Новости</NuxtLink>
+                    <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Услуги</NuxtLink>
+                    <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">FAQ</NuxtLink>
+                    <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Контактная информации</NuxtLink>
+                    <NuxtLink class="px-4 py-2 rounded-full bg-gray-100 text-[#12B1DE]">Войти</NuxtLink>
+                </nav>
+            </div>
+        </div>
     </header>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,182 @@
 <template>
-    <div>
-        123
+    <Services></Services>
+    <div class="flex flex-col gap-6">
+        <p class="text-2xl">Этапы покупки</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div class="flex flex-col gap-4 rounded-xl bg-white p-4 md:pb-10">
+                <p class="text-2xl"><span class="text-[#12B1DE]">01. </span>Выбор недвижимости</p>
+                <div class="flex flex-col">
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Недвижимость для любых целей</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>От надёжных застройщиков</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Служба индивидуальных показов</span>
+                    </p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4 rounded-xl bg-white p-4 md:pb-10">
+                <p class="text-2xl"><span class="text-[#12B1DE]">02. </span>Подготовка к сделке</p>
+                <div class="flex flex-col">
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Помощь в оформлении страхования</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Проверка документов</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Юридическая поддержка</span>
+                    </p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4 rounded-xl bg-white p-4 md:pb-10">
+                <p class="text-2xl"><span class="text-[#12B1DE]">03. </span>Сделка</p>
+                <div class="flex flex-col">
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Организация проведения сделки</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Присутствие менеджера на всех этапах сделки</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Курьерская служба</span>
+                    </p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4 rounded-xl bg-white p-4 md:pb-10">
+                <p class="text-2xl"><span class="text-[#12B1DE]">04. </span>ПОСТ-сопровождение</p>
+                <div class="flex flex-col">
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Контроль качества квартиры</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Информационная поддержка</span>
+                    </p>
+                    <p class="flex items-center gap-2">
+                        <span>—</span>
+                        <span>Помощь в продлении страховки</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="text-2xl">Преимущества</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div class="flex flex-col gap-8">
+                <div class="rounded-xl bg-white flex flex-col px-8 py-6">
+                    <p class="text-3xl">Сами</p>
+                    <p>С возможными рисками и без поддержки</p>
+                </div>
+                <div class="flex flex-col gap-4 p-4">
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Проверка жильцов</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl" name="material-symbols:close-small-rounded"/>
+                            <p>Везение и интуиция</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Фото</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl" name="material-symbols:close-small-rounded"/>
+                            <p>Как получится</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Неожиданный съезд жильцов</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl" name="material-symbols:close-small-rounded"/>
+                            <p>Начинаете всё с начала</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Стоимость</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl" name="material-symbols:close-small-rounded"/>
+                            <p>Ваше время и силы</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Договор аренды</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl" name="material-symbols:close-small-rounded"/>
+                            <p>Из интернета</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Бытовые проблемы</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl" name="material-symbols:close-small-rounded"/>
+                            <p>Решаете сами</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-8">
+                <div class="rounded-xl bg-[#12B1DE] flex flex-col px-8 py-6 text-white">
+                    <p class="text-3xl">С нами</p>
+                    <p>Мы бесплатно и быстро всё сделаем за вас</p>
+                </div>
+                <div class="flex flex-col gap-4 p-4">
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Проверка жильцов</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl text-[#12B1DE]" name="ic:outline-check"/>
+                            <p>Сделаем за вас</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Фото</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl text-[#12B1DE]" name="ic:outline-check"/>
+                            <p>Профессиональное фото бесплатно</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Неожиданный съезд жильцов</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl text-[#12B1DE]" name="ic:outline-check"/>
+                            <p>Найдём новых</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Стоимость</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl text-[#12B1DE]" name="ic:outline-check"/>
+                            <p>Бесплатно</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Договор аренды</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl text-[#12B1DE]" name="ic:outline-check"/>
+                            <p>Составлен юристами</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <p class="text-gray-400 ml-8 text-base">Бытовые проблемы</p>
+                        <div class="flex items-center gap-2">
+                            <Icon class="text-2xl text-[#12B1DE]" name="ic:outline-check"/>
+                            <p>Помогаем на протяжении всего срока</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
