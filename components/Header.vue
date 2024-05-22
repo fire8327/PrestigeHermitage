22562da19393b9052fdc186f8b15adb68fa5212a
fire8327 <fire83274@gmail.com>
2024-05-22 15:01:42 +0300

    upd header add adaptive

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-full grid-container py-6 relative bg-white rounded-b-3xl">
+    <header class="w-full grid-container py-6 relative bg-white lg:rounded-b-3xl">
         <div class="flex flex-col gap-4">
             <div class="flex items-center justify-between">
                 <NuxtLink to="/about" class="text-gray-400 text-base transition-all duration-500 hover:text-[#292929]">О компании</NuxtLink>
@@ -10,7 +10,7 @@
                 <NuxtLink to="/">
                     <img src="/images/header/logo.png" alt="" class="w-40">
                 </NuxtLink>
-                <nav class="flex items-center gap-6 text-lg">
+                <nav class="flex items-center gap-6 text-lg transition-all duration-500 max-lg:flex-col max-lg:absolute max-lg:w-full max-lg:left-0 max-lg:bg-white max-lg:py-6 max-lg:rounded-b-3xl z-[4]" :class="isMenuShow ? 'max-lg:top-full' : 'max-lg:top-0 max-lg:-translate-y-full'">
                     <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Поис квартир</NuxtLink>
                     <NuxtLink to="/news" class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Новости</NuxtLink>
                     <NuxtLink to="/services" class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Услуги</NuxtLink>
@@ -18,8 +18,12 @@
                     <NuxtLink to="/contacts" class="flex flex-col after:w-0 after:h-px after:bg-[#292929] after:transition-all after:duration-500 hover:after:w-full">Контакты</NuxtLink>
                     <NuxtLink class="px-4 py-2 rounded-full bg-gray-100 text-[#12B1DE]">Войти</NuxtLink>
                 </nav>
+                <button type="button" class="lg:hidden" @click="isMenuShow = !isMenuShow">
+                    <Icon class="text-3xl" name="ic:round-menu"/>
+                </button>
             </div>
         </div>
+        <div @click="isMenuShow = false" class="w-full col-span-full inset-0 bg-black/70 fixed left-0 top-36 lg:hidden z-[3]" :class="{'hidden' : !isMenuShow}"></div>
         <button type="button" @click="messageTitle = null" class="fixed top-10 right-10 z-[11] cursor-pointer flex items-center gap-2 px-6 py-2 text-lg rounded-2xl w-fit bg-white shadow-[0px_0px_13px_-7px_black]" :class="messageType ? ' text-[#12B1DE]/70' : 'text-red-500'" v-if="messageTitle">
             <Icon class="text-3xl" name="material-symbols:close-small-rounded"/>
             <span>{{messageTitle}}</span>
